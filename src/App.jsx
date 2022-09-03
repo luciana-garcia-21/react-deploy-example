c409954926b19f8d1b887ef9553e75d8d9b9e67d
luciana-garcia-21 <lucianagarciaortega.21@gmail.com>
2022-09-03 00:05:49 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import TaskList from './components/TaskList'
+import TaskFrom from './components/TaskFrom'
+
+function App() {
+
+  return (
+    <main className="bg-zinc-900 h-screen">
+       <div className="container mx-auto p-10">
+       <TaskFrom />
+       <TaskList />
+       </div>
+    </main>
+  );
+}
+
+export default App
